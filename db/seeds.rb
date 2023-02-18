1c1fa83e1d55af2c9bf206f3c719ab20d9fc7d5b
Dmitry Jum <xfiler@ymail.com>
2023-02-18 10:12:33 -0800

    updates the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-event1 = Event.create(name: "Rock Band festival", start_time: Time.zone.now + 3.days, end_time: Time.zone.now + 10.days, description: "Just some randome rock band playing", event_type: "in_person")
-event2 = Event.create(name: "Conference", start_time: Time.zone.now + 3.days, end_time: Time.zone.now + 10.days, description: "conversations and relaxations", event_type: "virtual")
-attendee1 = event2.attendees.create(name: "Vincent Descols", email: "vince@testmail.com", rsvp_status: "yes")
-attendee2 = event2.attendees.create(name: "Lance Wright", email: "lance@testmail.com")
+event1 = Event.new(name: "Rock Band festival", start_time: Time.zone.now + 3.days, end_time: Time.zone.now + 10.days, description: "Just some randome rock band playing", event_type: "in_person")
+event1.save!
+event2 = Event.new(name: "Conference", start_time: Time.zone.now + 3.days, end_time: Time.zone.now + 10.days, description: "conversations and relaxations", event_type: "virtual")
+event2.save!
+attendee1 = event1.attendees.create(name: "Vincent Descols", email: "vince@testmail.com", rsvp_status: "yes")
+attendee2 = event2.attendees.create(name: "Lance Wright", email: "vince@testmail.com")
