401b094ccbed43c3adcab68cfeacc522535fa737
Dmitry Jum <xfiler@ymail.com>
2023-02-19 18:39:46 -0800

    refactors request tests

--- spec/requests/attendees_controller_spec.rb
+++ spec/requests/attendees_controller_spec.rb
@@ -1,25 +1,23 @@
 require 'rails_helper'
 
 describe V1::AttendeesController do
-  before do
-    @event1 = FactoryBot.create :event,
-      name: "Event one",
-      start_time: Time.zone.parse("2022-03-12") + 3.days,
-      end_time: Time.zone.parse("2022-03-12") + 10.days,
-      description: FFaker::Lorem.paragraph,
-      event_type: "in_person"
-    @attendee1 = FactoryBot.create :attendee, name: "John Doe", rsvp_status: "yes", event_id: @event1.id
-  end
+  let!(:event1) { FactoryBot.create :event,
+    name: "Event one",
+    start_time: Time.zone.parse("2022-03-12") + 3.days,
+    end_time: Time.zone.parse("2022-03-12") + 10.days,
+    description: FFaker::Lorem.paragraph,
+    event_type: "in_person" }
+  let!(:attendee1) { FactoryBot.create :attendee, name: "John Doe", rsvp_status: "yes", event_id: event1.id }
 
   context "it gets an attendee by id" do
     it "finds an attendee by id" do
-      get v1_attendee_path(id: @attendee1.id)
-      expect(json_response['id']).to eq @attendee1.id
+      get v1_attendee_path(id: attendee1.id)
+      expect(json_response['id']).to eq attendee1.id
     end
 
     it "finds an attendee by friendly id" do
       get v1_attendee_path(id: "john-doe")
-      expect(json_response['id']).to eq @attendee1.id
+      expect(json_response['id']).to eq attendee1.id
     end
 
     it "doesn't find an event by id" do
@@ -32,7 +30,7 @@ describe V1::AttendeesController do
   context "it creates a new attendee (invitation record that needs to be rsvped if accepted)" do
     it "successfuly creates a new attendee" do
       test_attendee = {name: FFaker::Name.name, email: FFaker::Internet.email}
-      post v1_attendees_path(event_id: @event1.id, attendee: test_attendee)
+      post v1_attendees_path(event_id: event1.id, attendee: test_attendee)
       expect(response.status).to be 201
       expect(Attendee.count).to be 2
       expect(json_response["email"]).to eq(test_attendee[:email])
@@ -40,7 +38,7 @@ describe V1::AttendeesController do
 
     it "fails email validation" do
       test_attendee = {name: FFaker::Name.name}
-      post v1_attendees_path(event_id: @event1.id, attendee: test_attendee)
+      post v1_attendees_path(event_id: event1.id, attendee: test_attendee)
       expect(response.status).to be 422
       expect(json_response["email"].first).to eq "can't be blank"
     end
@@ -48,14 +46,14 @@ describe V1::AttendeesController do
 
   context "it updates an attendee" do
     it "successfuly updates an attendee" do
-      patch v1_attendee_path(id: @attendee1.id, attendee: {rsvp_status: "no"})
+      patch v1_attendee_path(id: attendee1.id, attendee: {rsvp_status: "no"})
       expect(response.status).to be 200
-      expect(json_response["rsvp_status"]).to eq(Attendee.find(@attendee1.id).rsvp_status)
+      expect(json_response["rsvp_status"]).to eq(Attendee.find(attendee1.id).rsvp_status)
     end
 
     it "fails to update an attendee" do
-      attendee2 = FactoryBot.create :attendee, event_id: @event1.id
-      patch v1_attendee_path(id: attendee2.id, attendee: {email: @attendee1.email})
+      attendee2 = FactoryBot.create :attendee, event_id: event1.id
+      patch v1_attendee_path(id: attendee2.id, attendee: {email: attendee1.email})
       expect(response.status).to be 422
       expect(json_response["email"].first).to eq "has already been taken"
     end
@@ -63,7 +61,7 @@ describe V1::AttendeesController do
 
   context "it destroys an attendee" do
     it "successfuly deletes an attendee" do
-      delete v1_attendee_path(id: @attendee1.id)
+      delete v1_attendee_path(id: attendee1.id)
       expect(response.status).to be 204
     end
   end
--- spec/requests/events_controller_spec.rb
+++ spec/requests/events_controller_spec.rb
@@ -1,27 +1,24 @@
 require 'rails_helper'
 
 describe V1::EventsController do
-  before do
-    @event1 = FactoryBot.create :event,
-      name: "Event one",
-      start_time: Time.zone.parse("2022-03-12") + 3.days,
-      end_time: Time.zone.parse("2022-03-12") + 10.days,
-      description: FFaker::Lorem.paragraph,
-      event_type: "in_person"
-    @event2 = FactoryBot.create :event,
-      name: "Event two",
-      start_time: Time.zone.parse("2022-03-12") + 4.days,
-      end_time: Time.zone.parse("2022-03-12") + 11.days,
-      description: FFaker::Lorem.paragraph,
-      event_type: "in_person"
-    @event3 = FactoryBot.create :event,
-      name: "Event three",
-      start_time: Time.zone.parse("2022-03-12") + 5.days,
-      end_time: Time.zone.parse("2022-03-12") + 12.days,
-      description: FFaker::Lorem.paragraph,
-      event_type: "in_person"
-    host! 'api.example.com'
-  end
+  let!(:event1) { create :event,
+    name: "Event one",
+    start_time: Time.zone.parse("2022-03-12") + 3.days,
+    end_time: Time.zone.parse("2022-03-12") + 10.days,
+    description: FFaker::Lorem.paragraph,
+    event_type: "in_person" }
+  let!(:event2) { create :event,
+    name: "Event two",
+    start_time: Time.zone.parse("2022-03-12") + 4.days,
+    end_time: Time.zone.parse("2022-03-12") + 11.days,
+    description: FFaker::Lorem.paragraph,
+    event_type: "in_person" }
+  let!(:event3) { create :event,
+    name: "Event three",
+    start_time: Time.zone.parse("2022-03-12") + 5.days,
+    end_time: Time.zone.parse("2022-03-12") + 12.days,
+    description: FFaker::Lorem.paragraph,
+    event_type: "in_person" }
 
   context "it gets a list of events" do
     it "requests all events by start time and receives paginated response" do
@@ -39,7 +36,7 @@ describe V1::EventsController do
     it "requests all events between the given start time and the end time and receives paginated response" do
       get v1_events_path(event: {start_time: "2022-03-16", end_time: "2022-03-24"}, per_page: 2)
       expect(json_response["entries_count"]).to be 2
-      expect(json_response["records"].map{|s| s["name"]}).to eq [@event2.name, @event3.name]
+      expect(json_response["records"].map{|s| s["name"]}).to eq [event2.name, event3.name]
     end
 
     it "receives response as the object with 'records', 'entries_count', 'pages_per_limit', 'page' keys" do
@@ -56,33 +53,33 @@ describe V1::EventsController do
     it "returns all events per requested page" do
       get v1_events_path(per_page: 2, page: 2)
       expect(json_response["records"].length).to eq 1
-      expect(json_response["records"].first["name"]).to eq @event3.name
+      expect(json_response["records"].first["name"]).to eq event3.name
     end
 
     it "requests 2nd page and forgets to specify the limit and receives 2nd or last page with default of max 9 records per page" do
       get v1_events_path(page: 2)
       expect(json_response["records"].length).to be 3
-      expect(json_response["records"].map{|s| s["name"]}).to eq [@event1.name, @event2.name, @event3.name]
+      expect(json_response["records"].map{|s| s["name"]}).to eq [event1.name, event2.name, event3.name]
     end
 
   end
 
   context "it gets an event by id" do
     it "finds an event by id" do
-      get v1_event_path(id: @event1.id)
-      expect(json_response['id']).to eq @event1.id
+      get v1_event_path(id: event1.id)
+      expect(json_response['id']).to eq event1.id
     end
 
     it "finds an event by friendly id" do
       get v1_event_path(id: "event-one")
-      expect(json_response['id']).to eq @event1.id
+      expect(json_response['id']).to eq event1.id
     end
 
     it "finds an event by id and the event json contains first page of attendee records that belong to this event" do
       10.times do
-        @event1.attendees.create(name: FFaker::Name.name, email: FFaker::Internet.email)
+        event1.attendees.create(name: FFaker::Name.name, email: FFaker::Internet.email)
       end
-      get v1_event_path(id: @event1.id, page: 1)
+      get v1_event_path(id: event1.id, page: 1)
       expect(json_response["attendees"]["records"].length).to eq 9
     end
 
@@ -102,7 +99,7 @@ describe V1::EventsController do
     end
 
     it "fails name validation" do
-      post v1_events_path(event: {name: @event3.name, description: "event with duplicate name", event_type: "in_person"})
+      post v1_events_path(event: {name: event3.name, description: "event with duplicate name", event_type: "in_person"})
       expect(response.status).to be 422
       expect(json_response["name"].first).to eq "has already been taken"
     end
@@ -110,13 +107,13 @@ describe V1::EventsController do
 
   context "it updates an event" do
     it "successfuly updates an event" do
-      patch v1_event_path(id: @event1.id, event: {name: "Event Ninety Nine"})
+      patch v1_event_path(id: event1.id, event: {name: "Event Ninety Nine"})
       expect(response.status).to be 200
-      expect(json_response["name"]).to eq(Event.find(@event1.id).name)
+      expect(json_response["name"]).to eq(Event.find(event1.id).name)
     end
 
     it "fails to update an event" do
-      patch v1_event_path(id: @event2.id, event: {name: @event1.name})
+      patch v1_event_path(id: event2.id, event: {name: event1.name})
       expect(response.status).to be 422
       expect(json_response["name"].first).to eq "has already been taken"
     end
@@ -124,22 +121,22 @@ describe V1::EventsController do
 
   context "it destroys an event" do
     it "successfuly deletes an event" do
-      delete v1_event_path(id: @event3.id)
+      delete v1_event_path(id: event3.id)
       expect(response.status).to be 204
     end
   end
 
   context "it rspvs to an event" do
     it "successfuly creates an attendee record with rsvp true if didn't exist" do
-      post rsvp_v1_event_path(id: @event3.id, attendee: {name: "John", email: "john@email.com"})
+      post rsvp_v1_event_path(id: event3.id, attendee: {name: "John", email: "john@email.com"})
       expect(response.status).to be 200
       expect(json_response["name"]).to eq "John"
       expect(json_response["rsvp_status"]).to eq "yes"
     end
 
     it "successfuly creates an attendee record with rsvp true if it already exist" do
-      attendee =  FactoryBot.create :attendee, event_id: @event1.id
-      post rsvp_v1_event_path(id: @event1.id, attendee: {name: attendee.name, email: attendee.email})
+      attendee =  FactoryBot.create :attendee, event_id: event1.id
+      post rsvp_v1_event_path(id: event1.id, attendee: {name: attendee.name, email: attendee.email})
       expect(response.status).to be 200
       expect(json_response["name"]).to eq attendee.name
       expect(json_response["rsvp_status"]).to eq "yes"
